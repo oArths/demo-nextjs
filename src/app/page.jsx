062e576385c9f783f72706aa13df3fe4530231df
Arthur <arthurferreira1417@gmail.com>
2024-08-10 22:06:08 -0300

    Feat: ajustando os tipos de arquivos pra sempre serem jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+import * as S from "./style.jsx";
+import Header from "@/components/headers";
+export default function Home() {
+  return (
+    <S.Body>
+      <Header />
+      home
+    </S.Body>
+  );
+}
